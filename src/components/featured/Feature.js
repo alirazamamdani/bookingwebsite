8bcc61ee43fb67b61ef9314b07c85369d8a15e3c
AliRazaMamdani <Ali.mamdani@hotmail.com>
2022-06-02 00:28:07 +0500

    featuredbox

--- src/components/featured/Feature.js
+++ src/components/featured/Feature.js
@@ -1,18 +1,46 @@
-import React from 'react'
-import './Feature.css'
+import React from "react";
+import "./Feature.css";
 
 const Feature = () => {
+  const featureItems = [
+    {
+      id: 1,
+      name: "City 1",
+      details: "lorem ipsum blah blah blah",
+      image:
+        "https://images.unsplash.com/photo-1449824913935-59a10b8d2000?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+    },
+    {
+      id: 2,
+      name: "City 2",
+      details: "lorem ipsum blah blah blah",
+      image:
+        "https://images.unsplash.com/photo-1649855908502-3f236ae18958?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8d29uZGVyZnVsJTIwY291bnRyaWVzfGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    },
+    {
+      id: 3,
+      name: "City 3",
+      details: "lorem ipsum blah blah blah",
+      image:
+        "https://images.unsplash.com/photo-1612293509840-afb0f18d417b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjd8fHdvbmRlcmZ1bCUyMGNvdW50cmllc3xlbnwwfDJ8MHx8&auto=format&fit=crop&w=500&q=60",
+    },
+  ];
+  console.log(featureItems);
   return (
-    <div className='Featured'>
-        <div className="FeatureItem">
-            <img src="https://cdn.pixabay.com/photo/2016/11/23/15/32/pedestrians-1853552__340.jpg" alt="" />
-            <div className='FeatureTitle'>
-                <h1>Dublin</h1>
-                <h4>123 Properties</h4>
+    <div className="FeaturedContainer">
+      {featureItems.map((item, i) => {
+        return (
+          <div className="FeatureItem" key={i}>
+            <img src={item.image} className="FeatureImg" alt="" />
+            <div className="FeatureTitle">
+              <h1>{item.name}</h1>
+              <h4>{item.details}</h4>
             </div>
-        </div>
+          </div>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default Feature
+export default Feature;
